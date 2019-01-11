ef92f15e519d6304ff35c013cace7eea22631378
DraciVik <viktor.stojanov90@gmail.com>
2019-01-11 23:22:04 +0100

    Npm installed onchange and parallelshell scripts

---
+++ Front-End-Web-UI-Frameworks-and-Tools-Bootstrap-4/js/scripts.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+  $("#mycarousel").carousel({ interval: 2000 });
+  $("#carouselButton").click(function() {
+    if (
+      $("#carouselButton")
+        .children("span")
+        .hasClass("fa-pause")
+    ) {
+      $("#mycarousel").carousel("pause");
+      $("#carouselButton")
+        .children("span")
+        .removeClass("fa-pause");
+      $("#carouselButton")
+        .children("span")
+        .addClass("fa-play");
+    } else if (
+      $("#carouselButton")
+        .children("span")
+        .hasClass("fa-play")
+    ) {
+      $("#mycarousel").carousel("cycle");
+      $("#carouselButton")
+        .children("span")
+        .removeClass("fa-play");
+      $("#carouselButton")
+        .children("span")
+        .addClass("fa-pause");
+    }
+  });
+  $("#loginLink").click(function() {
+    $("#loginModal").modal("show");
+  });
+  $("#reserveLink").click(function() {
+    $("#reserveModal").modal("show");
+  });
+});
