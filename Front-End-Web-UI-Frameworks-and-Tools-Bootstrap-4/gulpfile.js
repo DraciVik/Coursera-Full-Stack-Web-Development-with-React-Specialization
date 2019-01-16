a820ff62ac7e50e79f302392012c1034f2a3160d
DraciVik <viktor.stojanov90@gmail.com>
2019-01-16 21:27:56 +0100

    Finished with the gulp build part 2
    Gulp doesn't seem to work for me. Grunt worked just fine

--- Front-End-Web-UI-Frameworks-and-Tools-Bootstrap-4/gulpfile.js
+++ Front-End-Web-UI-Frameworks-and-Tools-Bootstrap-4/gulpfile.js
@@ -1,8 +1,16 @@
 "use strict";
 
-var gulp = require("gulp");
-let sass = require("gulp-sass");
-let browserSync = require("browser-sync");
+var gulp = require("gulp"),
+  sass = require("gulp-sass"),
+  browserSync = require("browser-sync"),
+  del = require("del"),
+  imagemin = require("gulp-imagemin"),
+  uglify = require("gulp-uglify"),
+  usemin = require("gulp-usemin"),
+  rev = require("gulp-rev"),
+  cleanCss = require("gulp-clean-css"),
+  flatmap = require("gulp-flatmap"),
+  htmlmin = require("gulp-htmlmin");
 
 gulp.task("sass", function() {
   return gulp
@@ -16,7 +24,8 @@ gulp.task("sass:watch", function() {
 });
 
 gulp.task("browser-sync", function() {
-  let files = ["./*.html", "./css/*.css", "./js/*.js", "./img/*.{png,jpg,gif}"];
+  var files = ["./*.html", "./css/*.css", "./img/*.{png,jpg,gif}", "./js/*.js"];
+
   browserSync.init(files, {
     server: {
       baseDir: "./"
@@ -24,4 +33,53 @@ gulp.task("browser-sync", function() {
   });
 });
 
-gulp.task("default", gulp.series("browser-sync", "sass:watch"));
+// Default task
+gulp.task("default", ["browser-sync"], function() {
+  gulp.start("sass:watch");
+});
+gulp.task("clean", function() {
+  return del(["dist"]);
+});
+
+gulp.task("copyfonts", function() {
+  gulp
+    .src("./node_modules/font-awesome/fonts/**/*.{ttf,woff,eof,svg}*")
+    .pipe(gulp.dest("./dist/fonts"));
+});
+
+// Images
+gulp.task("imagemin", function() {
+  return gulp
+    .src("img/*.{png,jpg,gif}")
+    .pipe(
+      imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })
+    )
+    .pipe(gulp.dest("dist/img"));
+});
+
+gulp.task("usemin", function() {
+  return gulp
+    .src("./*.html")
+    .pipe(
+      flatmap(function(stream, file) {
+        return stream.pipe(
+          usemin({
+            css: [rev()],
+            html: [
+              function() {
+                return htmlmin({ collapseWhitespace: true });
+              }
+            ],
+            js: [uglify(), rev()],
+            inlinejs: [uglify()],
+            inlinecss: [cleanCss(), "concat"]
+          })
+        );
+      })
+    )
+    .pipe(gulp.dest("dist/"));
+});
+
+gulp.task("build", ["clean"], function() {
+  gulp.start("copyfonts", "imagemin", "usemin");
+});
